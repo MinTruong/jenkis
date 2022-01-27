55f11c27ca2db6c4500cd238b98cf60b35fb46cb
MinTruong <86372849+MinTruong@users.noreply.github.com>
2022-01-27 14:16:00 +0700

    Update code.groovy

--- code.groovy
+++ code.groovy
@@ -2,6 +2,6 @@ import java.time.*
 import java.time.format.DateTimeFormatter
 
 def getVersionApp() {
-        sh(script: "head ${env.WORKSPACE}/pom.xml | grep  '<v' |grep -oP '(?<=\\>)[a-zA-Z0-9\\.-]*(?=\\<)'",returnStdout: true).trim()
+        sh(script: "head ${env.WORKSPACE}/pom.xml | grep  '<v' | grep -oP '(?<=\\>)[a-zA-Z0-9\\.-]*(?=\\<)' | cut -d '-' -f1",returnStdout: true).trim()
 }
 return this
