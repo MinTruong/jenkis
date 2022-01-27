16b30e12a6f58e3f050cbda672f261c34a08e9ee
MinTruong <86372849+MinTruong@users.noreply.github.com>
2022-01-27 13:18:44 +0700

    Delete code.groovy

--- code.groovy
+++
@@ -1,6 +0,0 @@
-import java.time.*
-import java.time.format.DateTimeFormatter
-
-def getVersionApp() {
-        sh(script: "head pom.xml | grep  '<v' |grep -oP '(?<=\>)[a-zA-Z0-9\.-]*(?=\<)'",returnStdout: true).trim()
-}
